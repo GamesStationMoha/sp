50d36d94f88315cb4348139e4a615f51109774bf
GamesStationMoha <45317067+GamesStationMoha@users.noreply.github.com>
2018-12-01 01:18:11 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,34 +1,17 @@
 ﻿const Discord = require('discord.js');
-const client = new Discord.Client();
+const1 client = new Discord.Client();
 var prefix = "1";
 
-client.on('ready', () => {
+client1.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);  
 });
 
-client.on('ready', async() => {
+client1.on('ready', async() => {
 var server = "498814210529034240"; // ايدي السررفر
 var channel = "517312143524364288";//ايدي الروم
     setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('**Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , **')
+    client1.guilds.get(server).channels.get(channel).send('**Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , **')
     },305);
 })
-
-client.on('message', message => {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
-
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice(1);
-
-  if (command == "say") {
-if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('?|**\`ADMINISTRATOR\`ليس لديك صلاحيات`**');
-   message.channel.sendMessage(args.join("  "))
-   message.delete()
-  }
- });
-
- 
 client.login(process.env.TOKEN1);
+
